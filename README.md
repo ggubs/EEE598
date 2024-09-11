e5ae3d2b7a2c18a4347ad45c9e6dc6b2d71a44e6
Gerry Gubash <ggubash@asu.edu>
2024-09-11 15:38:36 -0700

    stub out simulation for chernoff-hoeffding bound comparison

---
+++ README.md
@@ -0,0 +1,3 @@
+Various python scripts demonstrating concepts from ASU EEE598, Fall 2024:
+Machine Learning in Higher Dimensions
+
---
+++ python/hoeffding-chernoff.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def chernoff_bound() -> None:
+    """ Generate a bound using Chernoff's Inequality for the given parameters
+    """
+    pass
+
+def hoeffding_bound() -> None:
+    """ Generate a bound using Hoeffdings Inequality for the given parameters
+    """
+    pass
+
+def bernoulli(n:int, p:int) -> None:
+    """ Draw n times from a Bernoulli distrubition with parameter p
+
+    Args:
+        n (int): Number of draws
+        p (int): Bernoulli parameter
+    """
+    pass
+
+if __name__=="__main__":
+    # draw n times from bernoulli(p) distribution
+    pass
+    
+    # generate a chernoff bound
+    pass
+
+    # generate a hoeffding bound
+    pass
+
+    # plot the bounds against the distribution, inspect the tightness of each
+    pass
